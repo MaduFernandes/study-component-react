30bccd86170dfef49d466a8eee68f543d806bdad
AndersonAndrad <anderson_andrade_@outlook.com>
2021-04-13 21:15:03 -0300

    create input component
    creating the input component with the material UI, disregarding the use of CSS, source control may be necessary

--- src/components/Form/AccountForm.js
+++ src/components/Form/AccountForm.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React from 'react';
+import { Grid, FormControl } from '@material-ui/core';
+import { useStyles } from './style';
 
-import "../../css/index.css";
+import '../../css/index.css';
 
 const AccountForm = () => {
+  const classes = useStyles();
   return (
-    <div class="container-input">
-      <form id="form" noValidate autoComplete="off">
+    <Grid
+      container
+      xs="12"
+      sm="12"
+      className={classes.container}
+      direction="column"
+      justify="center"
+      alignContent="center"
+    >
+      <FormControl>
         <input
-          class="input-class"
-          id="input"
+          class={classes.input}
           type="email"
           placeholder="Email"
           autoComplete
         ></input>
         <input
-          class="input-class"
-          id="input"
+          class={classes.input}
           type="password"
           placeholder="Password"
           autoComplete
         ></input>
-      </form>
-    </div>
+      </FormControl>
+    </Grid>
   );
 };
 
--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,14 +1,26 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: theme.spacing(1),
-    width: "25ch",
-  },
-  input: {
-    width: "25em",
-    marginBottom: "15px",
-  },
-}));
+export const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
 
-export default useStyles;
+    container: {
+      width: '100%',
+      padding: '10px 20px',
+    },
+
+    input: {
+      width: '432px',
+      height: '62px',
+      borderRadius: '7px',
+      outline: 'none',
+      background: '#ffff',
+      border: 'none',
+      paddingLeft: '20px',
+      fontSize: '20px',
+    },
+  })
+);
