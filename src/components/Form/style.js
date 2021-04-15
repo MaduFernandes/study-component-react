0d85fb006e4ffdc91c359cdb6517147060f894b4
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2021-04-15 20:15:40 -0300

    feat: update file icons

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,26 +1,26 @@
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
 
     container: {
-      width: '100%',
-      padding: '10px 20px',
+      height: "300px",
     },
 
     input: {
-      width: '432px',
-      height: '62px',
-      borderRadius: '7px',
-      outline: 'none',
-      background: '#ffff',
-      border: 'none',
-      paddingLeft: '20px',
-      fontSize: '20px',
+      width: "432px",
+      height: "62px",
+      borderRadius: "7px",
+      outline: "none",
+      background: "#ffff",
+      border: "none",
+      paddingLeft: "20px",
+      fontSize: "20px",
+      margin: "20px",
     },
   })
 );
--- src/components/Icons/IconsForm.js
+++ src/components/Icons/IconsForm.js
@@ -5,15 +5,32 @@ import { MdEmail } from "react-icons/md";
 import { SiFacebook } from "react-icons/si";
 import { FaTwitter } from "react-icons/fa";
 
+//Material-ui
+import { Grid } from "@material-ui/core";
+
+//Styles
+import { useStyles } from "./style";
 import "../../css/index.css";
 
 const IconsForm = () => {
+  const classes = useStyles();
+
   return (
-    <div id="container-icon">
-      <MdEmail class="icon" />
-      <SiFacebook class="icon" />
-      <FaTwitter class="icon" />
-    </div>
+    <>
+      <Grid
+        container
+        xs="12"
+        sm="12"
+        className={classes.icon}
+        direction="row"
+        justify="center"
+        alignContent="center"
+      >
+        <MdEmail class="icon" />
+        <SiFacebook class="icon" />
+        <FaTwitter class="icon" />
+      </Grid>
+    </>
   );
 };
 
