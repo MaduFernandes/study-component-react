076753bae50cc8dc1540b0eab68db2fe4b2e0f6e
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2021-04-27 22:53:02 -0300

    feat: change name directory tests

--- src/tests/component/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../../App";
-
-test("Renders Create Account", () => {
-  render(<App />);
-  const response = screen.getByText(/Create Account/i);
-  expect(response).toBeInTheDocument();
-});
